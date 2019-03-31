5431c52f0e1fef9522f54a8218dc4cc45afb7b24
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-31 13:54:45 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -9,4 +9,12 @@ def reset!
   cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
 
+def display
+  puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+  puts "-----------"
+  puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+  puts "-----------"
+  puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+end  
+
 end
