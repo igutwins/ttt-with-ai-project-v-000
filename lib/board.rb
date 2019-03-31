e5bc8b25181d0f55f07f7f481b08be2a26910847
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-31 14:04:41 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,30 +1,43 @@
 class Board
-cells = []
 
-def initialize
-  reset!
-end
+  def initialize
+    reset!
+  end
 
-def reset!
-  cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-end
+  def reset!
+    cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
 
-def display
-  puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
-  puts "-----------"
-  puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
-  puts "-----------"
-  puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
-end
+  def display
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
 
-def position(input)
-  index = input.to_i - 1
-  cells[index]
-end
+  def position(input)
+    index = input.to_i - 1
+    cells[index]
+  end
 
-def update(position,object)
-  position(postion) = object
+  def update(position,object)
+    position(postion) = object
+  end
 
-end
+  def token
+
+  end
+
+  def taken?
+
+  end
+
+  def valid_move?
+
+  end
+
+  def turn_count
+  end
 
 end
