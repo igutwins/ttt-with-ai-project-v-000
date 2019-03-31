294d3f4c6966f4ebed548ce2a522fac13c4f3d93
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-31 15:11:43 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -14,10 +14,10 @@ WIN_COMBINATIONS = [
 
 
 
-def initialize(player_1, player_2, board)
-  board = Board.new
-  player_1 = Player.new("X")
-  player_2 = Player.new("O")
+def initialize(player_1 = Players:Human.new("X"), player_2 = Players:Human.new("O"), board = Board.new)
+  @board = board
+  @player_1 = player_1
+  @player_2 = player_2
 end
 
 end
